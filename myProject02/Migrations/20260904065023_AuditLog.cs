f39164b19ed38a15d431f921a7d4a6275361b510
Anfas Arshad <anfasarshad@gmail.com>
2026-09-04 15:44:26 +0530

    "update code"

---
+++ myProject02/Migrations/20260904065023_AuditLog.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace myProject02.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuditLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Voter",
+                table: "Voter");
+
+            migrationBuilder.RenameTable(
+                name: "Voter",
+                newName: "Voters");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Voters",
+                table: "Voters",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Audit",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EntityName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Action = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Changes = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Audit", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Audit");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Voters",
+                table: "Voters");
+
+            migrationBuilder.RenameTable(
+                name: "Voters",
+                newName: "Voter");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Voter",
+                table: "Voter",
+                column: "Id");
+        }
+    }
+}
--- myProject02/Program.cs
+++ myProject02/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using MyBackendApp.Services;
 using myProject02.Models;
+using myProject02.Services;
 
 namespace myProject02
 {
@@ -8,6 +10,8 @@ namespace myProject02
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddScoped<Ivoterservice, VoterService>();
 
             // Add services to the container.
 
