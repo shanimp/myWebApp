f37070e637c06a98feb751b47646ad655b3dae4e
Anfas Arshad <anfasarshad@gmail.com>
2026-09-04 12:02:54 +0530

    first commit

---
+++ myProject02/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System.Security.Cryptography.X509Certificates;
+
+namespace myProject02.Models
+
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+           
+    }
+        public DbSet<Voter> Voters { get; set; }
+
+    }
+}
---
+++ myProject02/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using myProject02.Models;
+
+namespace myProject02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("VoterConnection")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
